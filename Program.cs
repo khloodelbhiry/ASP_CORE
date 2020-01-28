e0f1a441893bd0478804da637ab87fa183b9e5ed
khloodelbhiry <khloud@fditc.net>
2020-01-28 15:28:58 +0200

    upload project

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace MostFrequent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter array values sperated by , ");
+            string[] a = Array.ConvertAll(Console.ReadLine().ToString().Split(','), Convert.ToString);
+            Console.WriteLine("Result is : " + GetMostFrequent(a));
+        }
+        private static string GetMostFrequent(string[] arr)
+        {
+            var query = arr.GroupBy(arr => arr).OrderByDescending(x => x.Count()).Select(x => x.Key);
+            return query.FirstOrDefault();
+        }
+    }
+}
